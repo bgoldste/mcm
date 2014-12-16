893c76009d8c87fae375521358177aa4123ccc2a
bgoldste <bengoldstein9@gmail.com>
2014-12-16 01:39:54 -0800

    submit time

--- background.js
+++ background.js
@@ -1,25 +1,79 @@
 console.log('background running')
 
-var data = {};
-console.log('data = ')
-console.log(data);
 
-chrome.storage.sync.get(['userid', 'goalid'], function(result){
-	console.log('checking for info in storage       ' + typeof result.userid)
-	
+//initialize data and state by calling from storage, and setting to defaults if not found.
+var data; 
 
-	if (result.userid === undefined) {
-		console.log('user id not found in storage, starting login')
-		// chrome.tabs.create({url:"popup.html"});
-		//login()
+var state;
+
+var startTime;
+
+chrome.storage.sync.get(['state', 'data', 'startTime'], function(result){
+	if(result.state){
+		state = result.state;
 	}
-	else {
-		console.log('updating local var from storage');
-		data.goalid = result.goalid;
-		data.userid = result.userid;
+	else{
+		state = 'not_logged_in';
 	}
+
+	if(result.data){
+		data = result.data;
+	}
+	else{
+		data = {}
+	}
+
+	if(result.startTime){
+		startTime = result.startTime;
+	}
+	else{
+		startTime = new Date();
+	}
+
+
 });
 
+
+
+var logout = function(){
+	data = {};
+	state = 'not_logged_in';
+	chrome.storage.sync.set({'state': 'not_logged_in', 'data': {}})
+};
+
+
+
+var startTimer = function  () {
+	if (data.startTime === undefined){
+		startTime = new Date();
+		data.startTime = startTime;
+		chrome.storage.sync.set({'startTime': data.startTime});
+		refreshPopup();
+	}
+};
+
+
+
+var submitTime = function(elapsedTime){
+	state = 'submit_time';
+	data.elapsedTime = elapsedTime;
+	chrome.storage.sync.set({'elapsedTime': data.startTime, 'state': 'submit_time'});
+	refreshPopup();
+
+
+};
+
+
+
+
+var refreshPopup = function() {
+    for (var i = 0; i < chrome.extension.getViews().length; i++){
+	    if(chrome.extension.getViews()[i].location.href != window.location.href){
+	   		chrome.extension.getViews()[i].location.reload();
+		}
+	}
+
+}
 
 
 
@@ -46,29 +100,25 @@ var login = function(username, password){
 	})
 	.success(function( response ) {
 			
-			console.log('success ! data: ' + 'userid ' +  response.userid + 'data goal ' + response.goals[0].id);
-
+			//console.log('success ! data: ' + 'userid ' +  response.userid + 'data goal ' + response.goals[0].id);
 			if(response.userid && response.goals[0].id){
-				alert('storing data.userid! ' + response.userid + 'data goal ' + response.goals[0].id);
-				chrome.storage.sync.set({'userid': response.userid, 'goalid': response.goals[0].id}, function() {
-			          // Notify that we saved.
-			          console.log('storing userid and goal');
-			    });
+				console.log('storing data.userid! ' + response.userid + 'data goal ' + response.goals[0].id);
+				// chrome.storage.sync.set({'userid': response.userid, 'goalid': response.goals[0].id}, function() {
+			 //          // Notify that we saved.
+			 //          console.log('storing userid and goal');
+			 //    });
 			    data.userid = response.userid;
 			    data.goalid = response.goals[0].id;
-
-
+			    state = 'logged_in';
+			    chrome.storage.sync.set({'state': 'logged_in', 'data': {'userid': response.userid, 'goalid': response.goals[0].id}})
+			    refreshPopup();
 			}
 			else if (data.userid){
-				alert("no  goal!");
-				chrome.storage.sync.set({'userid': data.userid}, function() {
-			          // Notify that we saved.
-			          console.log('only able to store userid');
-			         
-			    });
+				console.log("no  goal!");
+				
 			}
 			else{
-				alert('no goal or uid found');
+				console.log('no goal or uid found');
 				console.log('nothing saved');
 			}
 			
@@ -77,44 +127,3 @@ var login = function(username, password){
 
 	});
 };
-
-
-// var login = function(){
-// 	jQuery.ajax({
-// 	  type: "POST",
-// 	  url: "https://salty-inlet-9116.herokuapp.com/login_json/",
-
-// 	  data : {'username': 'ben', 'password': '1'},
-// 	  dataType: 'json',
-// 	}) 
-	
-// 	.error(function(err){
-// 		console.log('error' + err);
-
-// 	})
-// .success(function( data ) {
-		
-// 		console.log('success ! data: ' + data.user);
-// 		  });
-// }();
-
-// var select_goal = function(){
-// 	jQuery.ajax({
-// 	  type: "POST",
-// 	  url: "https://salty-inlet-9116.herokuapp.com/select_goal/",
-
-// 	  data : {},
-// 	  dataType: 'json',
-// 	}) 
-	
-// 	.error(function(err){
-// 		console.log('error' + err);
-
-// 	})
-// .success(function( data ) {
-		
-// 		console.log('success ! data: ' + data.user);
-// 		console.log('msg: ' + data.msg)
-// 		  });
-// }();
-
--- popup.js
+++ popup.js
@@ -18,32 +18,131 @@
 
 
 
-document.addEventListener('DOMContentLoaded', function () {
-	// // alert('from event listener');
-	// moocbGenerator.testFunction()
-	data = chrome.extension.getBackgroundPage().data
-	if (data.userid && data.goalid){
-		$('form').remove()
+document.addEventListener('DOMContentLoaded', function() {
+	state = chrome.extension.getBackgroundPage().state;
+	data = chrome.extension.getBackgroundPage().data;
+	console.log(state);
+	if(state === 'not_logged_in' || state === undefined){
+		$('h1').css("color", "red");
+		$(' button#login ').click( function(){
+			username =  $('[name="username"]').val();
+			password = $('[name="password"]').val();
+			chrome.extension.getBackgroundPage().login(username, password);	
+		});
+
+	}
+
+
+	if(state === 'submit_time'){
+		$('form').hide();
+		$('.user_content').append('<h1>Submit your time of : ' + data.elapsedTime + '!</h1>');
+
+	}
+
+
+
+
+
+	if(state === 'logged_in'){
+		$('h1').css("color", "blue");
+		$('form').hide();
 		$('.user_content').append('<h1>You are logged in </h1>');
 		$('.user_content').append('<h1> User Id: ' +data.userid +   '</h1>');
 
 		$('.user_content').append('<h1> Goal Id :' +data.goalid +   '</h1>');
+
+		if(data.startTime){
+			//now = new Date() 
+			$('.timer').append('<h3 id = "elapsed"> Elapsed Time: seconds </h3>');
+			setInterval(function(){ 
+				elapsedTime = (new Date()- data.startTime)/1000; 
+
+				$('#elapsed').html('Elapsed Time: ' + elapsedTime + ' seconds');
+			}, 200);
+
+
+
+			$('.timer').append('<button id="submitTime">Submit your time</button>');
+			$('#submitTime').click(function(){
+				console.log('submitting time ' + elapsedTime);
+				chrome.extension.getBackgroundPage().submitTime(elapsedTime);
+			})
+
+		}
+		else{
+			$('.timer').append('<button id = "timer">Start Time</button');
+			$('#timer').click(function(){
+				chrome.extension.getBackgroundPage().startTimer();
+			});
+		}
+
+		$('.user_content').append('<a id= "logout" href = "">logout</a>');
+		$('#logout').click(function(){
+			chrome.extension.getBackgroundPage().logout();
+		});
 	}
 
-	else{
 
 
-	$(' button#login ').click( function(){
-		username =  $('[name="username"]').val();
-		password = $('[name="password"]').val();
-		$('h1').text('')
-		chrome.extension.getBackgroundPage().login(username, password)
-		//login()
-	});
 
 
-	}
-	$('h1').css("color", "#A3A3F3");
+	// // alert('from event listener');
+	// // moocbGenerator.testFunction()
+	// console.log('getting data from background now');
+	// data = chrome.extension.getBackgroundPage().data
+
+
+	// if (data.userid && data.goalid > 0 ){
+	// 	//window.location.href='popup.html';
+	// 	$('form').remove()
+	// 	$('.user_content').append('<h1>You are logged in </h1>');
+	// 	$('.user_content').append('<h1> User Id: ' +data.userid +   '</h1>');
+
+	// 	$('.user_content').append('<h1> Goal Id :' +data.goalid +   '</h1>');
+	// 	$('.user_content').append('<a id= "logout" href = "">logout</a>');
+	// 	$('#logout').click(function(){
+	// 		var logoutCallback = function() {
+	// 			console.log('running logout call back');
+	// 			chrome.extension.getBackgroundPage().data = {};
+	// 			//setTimeout(function(){window.location.reload();}, 700);
+				
+	// 		}();
+
+	// 	})
+	// }	
+
+	// else{
+
+
+	// $(' button#login ').click( function(){
+	// 	username =  $('[name="username"]').val();
+	// 	password = $('[name="password"]').val();
+	// 	$('h1').text('')
+	// 	console.log('running login');
+	// 	$('.user_content').append('<h1>You are logged in </h1>');
+	// 	$('.user_content').append('<h1> User Id: ' +data.userid +   '</h1>');
+
+	// 	$('.user_content').append('<h1> Goal Id :' +data.goalid +   '</h1>');
+	// 	$('.user_content').append('<a id= "logout" href = "">logout</a>');
+	// 	var login2 = function(callback){
+	// 		chrome.extension.getBackgroundPage().login(username, password);
+	// 		callback()
+	// 	};
+	// 	var loginCallback = function() {
+	// 		console.log('running login call back');
+	// 		//chrome.extension.getBackgroundPage().login(username, password);
+	// 		$('form').hide()
+	// 		window.location.reload();
+			
+	// 	};
+	// 	login2(loginCallback);
+	// 	console.log('finished login');
+		
+	// });
+
+
+	// }
+	// $('h1').css("color", "#A3A3F3");
 
 
 
